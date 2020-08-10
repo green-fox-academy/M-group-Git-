62f0376548386ba0582f944392c646c8bfe7a3e7
Mozsolics Dániel <d.mozsolics@gmail.com>
2020-08-10 14:18:25 +0200

    Main restore

--- src/git_project/Main.java
+++ src/git_project/Main.java
@@ -1,26 +1,9 @@
 package git_project;
 
 public class Main {
-    public static void main(String[] args) {
-        Future future = new Future();
-        future.future();
-    }
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        cheer();
-        Cheer cheerOne = new Cheer();
-        cheerOne.cheer();
-    }
-
-    public static void cheer () {
-        System.out.println("Hello Group M!");
-    }
-
-
-=======
 
     }
-
->>>>>>> 1d4cff3d653a90c1e4d7dfefda405ef4aad58ac5
 }
+
