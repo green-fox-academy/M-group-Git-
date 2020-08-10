a29690f8fa28a7432ff72b576a25ca1feddc201d
Moni Ferenczi <ferenczimonika91@gmail.com>
2020-08-10 15:16:34 +0200

    c5

--- src/git_project/Count.java
+++ src/git_project/Count.java
@@ -3,9 +3,9 @@ package git_project;
 public class Count {
 
 
-  public void count(int age, String name){
-    age = 2;
-    System.out.println(age);
+  public void count(int parameter, String name){
+    //age = 2;
+    System.out.println("Counting the countless "+parameter);
     System.out.println("name: " + name);
   }
 }
