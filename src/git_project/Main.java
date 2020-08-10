f7c2a6a26fc23b95e4c8aacdbe5dc8ba8f9ef298
greszter <esztergreskovics@gmail.com>
2020-08-10 14:01:54 +0200

    b1

--- src/git_project/Main.java
+++ src/git_project/Main.java
@@ -2,4 +2,15 @@ package git_project;
 
 public class Main {
 
+  public static void main(String[] args) {
+
+    count();
+
+  }
+
+
+  public static void count(){
+    System.out.println(35);
+  }
+
 }
